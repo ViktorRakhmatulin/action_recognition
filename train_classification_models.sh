6e23ca262914540842ab683031ea9b7f017bdfd9
ViktorRakhmatulin <vikt-atl30@ya.ru>
2022-03-27 12:52:55 +0300

    Update train_classification_models.sh
    Training pipeline is uncommented

--- train_classification_models.sh
+++ train_classification_models.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Runs LSTM hyperparams training/evaluation pipeline (biderectional /unidir LSTM units, Linear units)
-#for DIR in '-u' '-b'
-#do
-#  for st in 1 2
-#    do
-#      for lr in 1 2
-#      do
-#          echo $DIR $st $lr
-#          python train_classify.py --run-dir runs_classification \
-#              data/HDM05-15/HDM05-15-part1.pkl\
-#              data/HDM05-15/HDM05-15-part2.pkl $DIR -s $st -l $lr
-#        done
-#    done
-#done
+for DIR in '-u' '-b'
+do
+ for st in 1 2
+   do
+     for lr in 1 2
+     do
+         echo $DIR $st $lr
+         python train_classify.py --run-dir runs_classification \
+             data/HDM05-15/HDM05-15-part1.pkl\
+             data/HDM05-15/HDM05-15-part2.pkl $DIR -s $st -l $lr
+       done
+   done
+done
 
 #Run HDM05-122 dataset
 #python train_classify.py --run-dir runs_classification \
